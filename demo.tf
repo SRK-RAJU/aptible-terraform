7ba1e50fe37d6bff2f55d44bde194a2e336c11bd
RAJUKUMAR SUDDALA <97968060+SRK-RAJU@users.noreply.github.com>
2023-07-31 17:19:45 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -2,17 +2,17 @@
 # https://www.aptible.com/documentation/deploy/tutorials/deploy-demo-app.html
 
 # TODO: Enter your account handle here
-data "aptible_environment" "demo" {
-  handle = "env-qa"
+data "aptible_environment" "env" {
+  handle = "env-test"
 }
 
-resource "aptible_app" "demo-app" {
-  env_id = data.aptible_environment.demo.env_id
-  handle = "demo-app"
+resource "aptible_app" "env-test" {
+  env_id = data.aptible_environment.env.env_id
+  handle = "env-test"
   config = {
     "APTIBLE_DOCKER_IMAGE" = "quay.io/aptible/deploy-demo-app"
-    "DATABASE_URL"         = aptible_database.demo-pg.default_connection_url
-    "REDIS_URL"            = aptible_database.demo-redis.default_connection_url
+    "DATABASE_URL"         = aptible_database.env-pg.default_connection_url
+    "REDIS_URL"            = aptible_database.env-redis.default_connection_url
   }
   service {
     process_type           = "web"
@@ -30,31 +30,31 @@ resource "aptible_app" "demo-app" {
   # If you need to run migrations on each app release, you should use a before_release command
   # https://www.aptible.com/documentation/deploy/reference/apps/aptible-yml.html#before-release
   provisioner "local-exec" {
-    command = "aptible ssh --app demo-app python migrations.py"
+    command = "aptible ssh --app env-test python migrations.py"
   }
 }
 
-resource "aptible_database" "demo-pg" {
-  env_id         = data.aptible_environment.demo.env_id
-  handle         = "demo-pg"
+resource "aptible_database" "env-pg" {
+  env_id         = data.aptible_environment.env.env_id
+  handle         = "env-pg"
   database_type  = "postgresql"
   container_size = 512
   disk_size      = 10
 }
 
-resource "aptible_database" "demo-redis" {
-  env_id         = data.aptible_environment.demo.env_id
-  handle         = "demo-redis"
+resource "aptible_database" "env-redis" {
+  env_id         = data.aptible_environment.env.env_id
+  handle         = "env-redis"
   database_type  = "redis"
   container_size = 512
   disk_size      = 10
 }
 
-resource "aptible_endpoint" "demo-app-public-endpoint" {
-  env_id         = data.aptible_environment.demo.env_id
+resource "aptible_endpoint" "env-app-public-endpoint" {
+  env_id         = data.aptible_environment.env.env_id
   resource_type  = "app"
   process_type   = "web"
-  resource_id    = aptible_app.demo-app.app_id
+  resource_id    = aptible_app.env-test.app_id
   default_domain = true
   endpoint_type  = "https"
   container_port = 5000

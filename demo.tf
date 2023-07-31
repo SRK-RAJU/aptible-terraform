fc61235d3fa44da6c4ed9c13a6232e088438b03c
RAJUKUMAR SUDDALA <97968060+SRK-RAJU@users.noreply.github.com>
2023-07-31 20:13:21 +0530

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -8,14 +8,6 @@
 #  handle = "env-test"
 #}
 
-terraform {
-  required_providers {
-    aptible = {
-      source = "aptible/aptible"
-       version = "0.7.3"
-    }
-  }
-}
 
 resource "aptible_environment" "env-test" {
     org_id = "aa25fb77-4eef-4b83-818c-c35395d7ee86"

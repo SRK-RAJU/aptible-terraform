5317c93b9f801f4bc396ae6b574af5a66a0acf66
RAJUKUMAR SUDDALA <97968060+SRK-RAJU@users.noreply.github.com>
2023-07-31 20:13:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
-#terraform {
- # required_providers {
-#    aptible = {
- #     source = "aptible/aptible"
- #     version = "0.7.3"
-#    }
-#  }
-#}
+terraform {
+ required_providers {
+   aptible = {
+     source = "aptible/aptible"
+     version = "0.7.3"
+    }
+  }
+}
 
-#provider "aptible" {
-#}
+provider "aptible" {
+}
